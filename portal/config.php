f2730715fd7c05e493f212df3b6ab5e6624f7646
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-08-30 04:59:41 +0530

    Admin Panel Pages

--- portal/config.php
+++ portal/config.php
@@ -4,10 +4,10 @@ define("PATH", "/portal");
 define("PATH_APP", "/app");
 define("KB", DOMAIN.PATH."/kb");
 define("ABS_PATH", dirname(__FILE__));
-define("DB_NAME", "");
-define("DB_HOST", "");
+define("DB_NAME", "iitr");
+define("DB_HOST", "tbsdbinstance.ctlrgvtx2zlb.ap-south-1.rds.amazonaws.com");
 define("DB_PORT", "3306");
-define("DB_LOGIN", "");
-define("DB_PASS", "");
-define("TITLE", "IITR Logistics");
-?>
+define("DB_LOGIN", "iitr");
+define("DB_PASS", "password");
+define("TITLE", "IITR Document Tracking System");
+define("ADMIN","/admin");
