d22f42654cc6fa4e074da56a7408fdf301941445
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-08-30 07:29:53 +0530

    Delete User Minor bug fix

--- portal/api/delete_user.php
+++ portal/api/delete_user.php
@@ -10,6 +10,7 @@ $id=(new Sessions())->getID();
 if((new Users($id))->isAdmin){
     if(isset($_POST['id'])&&$_POST['id']!=""&&$_POST['id']!=$id){
         (new DB())->forgetdb("users",array("enrlid"=>$_POST['id']));
+        (new DB())->forgetdb("sessions",array("eid"=>$_POST['id']));
         echo 1;
     }
     else echo 0;
