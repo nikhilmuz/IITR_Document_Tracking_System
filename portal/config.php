f4ac8119bd285f696eea0aaa8a1a5f9646b12ccd
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-09-01 12:00:07 +0530

    Update config.php

--- portal/config.php
+++ portal/config.php
@@ -1,11 +1,11 @@
 <?php
-define("DOMAIN", "http://localhost:8080");
+define("DOMAIN", "http://iitr.nikhilkumar.ga");
 define("PATH", "/portal");
 define("PATH_APP", "/app");
 define("KB", DOMAIN.PATH."/kb");
 define("ABS_PATH", dirname(__FILE__));
 define("DB_NAME", "iitr");
-define("DB_HOST", "tbsdbinstance.ctlrgvtx2zlb.ap-south-1.rds.amazonaws.com");
+define("DB_HOST", "sql.nikhilkumar.ga");
 define("DB_PORT", "3306");
 define("DB_LOGIN", "iitr");
 define("DB_PASS", "password");
