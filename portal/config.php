7db6816f43d1d4b8aca51829423c56a26e1dd433
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-09-03 07:48:34 +0530

    Anonymous tracking+

--- portal/config.php
+++ portal/config.php
@@ -6,7 +6,7 @@ define("PATH_APP", "/app");
 define("KB", DOMAIN.PATH."/kb");
 define("ABS_PATH", dirname(__FILE__));
 define("DB_NAME", "iitr");
-define("DB_HOST", "sql.nikhilkumar.ga");
+define("DB_HOST", "sql.nikhilkuamr.ga");
 define("DB_PORT", "3306");
 define("DB_LOGIN", "iitr");
 define("DB_PASS", "password");
