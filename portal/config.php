9b80a057d7f382a86474e9030d8f0f6c569c1dde
Nikhil Kumar <nikhil.nikhil.muz@gmail.com>
2018-09-03 07:19:22 +0530

    Dynamic Office Creation

--- portal/config.php
+++ portal/config.php
@@ -1,11 +1,12 @@
 <?php
-define("DOMAIN", "http://localhost:8080");
+//define("DOMAIN", "http://localhost:8080");
+define("DOMAIN", "http://sql.nikhilkumar.ga");
 define("PATH", "/portal");
 define("PATH_APP", "/app");
 define("KB", DOMAIN.PATH."/kb");
 define("ABS_PATH", dirname(__FILE__));
 define("DB_NAME", "iitr");
-define("DB_HOST", "tbsdbinstance.ctlrgvtx2zlb.ap-south-1.rds.amazonaws.com");
+define("DB_HOST", "sql.nikhilkuamr.ga");
 define("DB_PORT", "3306");
 define("DB_LOGIN", "iitr");
 define("DB_PASS", "password");
